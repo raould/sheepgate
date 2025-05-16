61dfd73202cc69213e0c31086738cec5dad58c6e
Raoul Duke <raould@gmail.com>
2025-05-15 22:34:56 -0700

    checkpoint

--- OLD/client/src/client.ts
+++ OLD/client/src/client.ts
@@ -30,7 +30,7 @@ function assert(test: boolean, msg: string = "") {
 
 // so i can have everything in dark mode on my Windows machine.
 // note that this kills Edge fps, but works ok with Firefox, whatevz!!!
-const INVERT_COLORS = true;
+const INVERT_COLORS = false;
 
 // todo: use the server types.
 let server_db_generation: { id: number; db: any; } = { id: 0, db: undefined };
