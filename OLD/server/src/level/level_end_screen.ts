29fb804a841da97a2ab665555904e503dd4dcfd3
Raoul Duke <raould@gmail.com>
2025-05-16 07:21:31 -0700

    checkpoint

--- OLD/server/src/level/level_end_screen.ts
+++ OLD/server/src/level/level_end_screen.ts
@@ -5,7 +5,7 @@ const KCART1_SFX = { sfx_id: K.KCART1_SFX, gain: 0.3, singleton: true };
 
 export class LevelEndScreen extends Is.InstructionsScreen {
     constructor(spec: Is.InstructionsScreenSpec) {
-	super(spec);
+	super({ ...spec, timeout: spec.timeout ?? 10*1000 });
 	this.mdb.items.sfx.push({ sfx_id: K.SYNTH_A_SFX });
     }
 
