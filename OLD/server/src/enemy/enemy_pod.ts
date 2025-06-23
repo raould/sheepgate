624a1033d610d2519f42a4d9a53554edebede32f
Raoul Duke <raould@gmail.com>
2025-06-23 00:04:40 -0700

    checkpoint

--- OLD/server/src/enemy/enemy_pod.ts
+++ OLD/server/src/enemy/enemy_pod.ts
@@ -85,7 +85,7 @@ function t2a_facing_mk(db: GDB.GameDB, thrusting: boolean, facing: F.Facing): A.
 	    animator: A.animator_mk(
                 db.shared.sim_now,
                 {
-		    frame_msec: 40,
+		    frame_msec: 80,
 		    resource_ids: [
                         ...images.lookup_range_n(n => `enemies/pods/sprite_${n}.png`, 0, 3)
 		    ],
