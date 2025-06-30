29f5ee09b50a1517acf8596ab63f23e36c431253
Raoul Duke <raould@gmail.com>
2025-06-29 19:41:11 -0700

    wip

--- OLD/server/src/konfig.ts
+++ OLD/server/src/konfig.ts
@@ -6,7 +6,8 @@ import { RGBA } from './color';
 
 export const ARCADE_MODE = true;
 
-export const DEBUG_HACK_ONLY_HYPERMEGA = true;
+// super testing hack! beware!
+export const DEBUG_HACK_ONLY_HYPERMEGA = false;
 
 // todo: what would be better is to have types for the different
 // kinds of values, and to have all this in a dict, and then
