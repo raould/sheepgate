2f5b6aa0eceb03bbbc6021dcc43a78654152c0f2
Raoul Duke <raould@gmail.com>
2025-07-04 14:51:08 -0700

    wip

--- OLD/server/src/enemy/enemy_mk.ts
+++ OLD/server/src/enemy/enemy_mk.ts
@@ -62,7 +62,7 @@ function warpin_mk(db: GDB.GameDB, size: G.V2D, resource_id: string, spec: Enemy
 	    }
 	);
     }
-    const lt = Eu.safe_lt(db, spec.rank, size, Rnd.singleton, db.shared.items.player!.lt);
+    const lt = Eu.safe_lt(db, spec.rank, size, Rnd.singleton, lt); //db.shared.items.player!.lt);
     const rect = G.rect_mk(lt, size);
     spec.lt = lt;
     DebugGraphics.add_rect(DebugGraphics.get_permanent(), rect);
