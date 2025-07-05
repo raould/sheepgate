f0c1a5d3762eb64a0de88e6c6d65b78e3a76567f
Raoul Duke <raould@gmail.com>
2025-07-05 14:35:26 -0700

    wip

--- OLD/server/src/game.ts
+++ OLD/server/src/game.ts
@@ -67,15 +67,16 @@ type LevelMk = (level_index: number, score: number, high_score: Hs.HighScore) =>
 // match: konfig.ts
 const level_mks: LevelMk[] = [
     // note that the first level is special-cased in various places e.g. people populating.
-    (i: number, score: number, hi: Hs.HighScore) => Lzx.level_mk(i, score, hi),
-    (i: number, score: number, hi: Hs.HighScore) => Lcbm.level_mk(i, score, hi),
     (i: number, score: number, hi: Hs.HighScore) => L1.level_mk(i, score, hi),
     (i: number, score: number, hi: Hs.HighScore) => L2.level_mk(i, score, hi),
     (i: number, score: number, hi: Hs.HighScore) => L3.level_mk(i, score, hi),
+    (i: number, score: number, hi: Hs.HighScore) => Lcbm.level_mk(i, score, hi),
     (i: number, score: number, hi: Hs.HighScore) => L4.level_mk(i, score, hi),
     (i: number, score: number, hi: Hs.HighScore) => L5.level_mk(i, score, hi),
     (i: number, score: number, hi: Hs.HighScore) => L6.level_mk(i, score, hi),
+    (i: number, score: number, hi: Hs.HighScore) => Lzx.level_mk(i, score, hi),
     (i: number, score: number, hi: Hs.HighScore) => L7.level_mk(i, score, hi),
+
 ];
 //D.assert(level_mks.length === K.LEVEL_TEMPLATE_COUNT, "level template count");
 
