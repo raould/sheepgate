49b1e9e52678a87ffcb75245806c7d9ed5b5de68
Raoul Duke <raould@gmail.com>
2025-07-10 18:54:36 -0700

    wip

--- OLD/server/src/debug.ts
+++ OLD/server/src/debug.ts
@@ -1,4 +1,10 @@
 /* Copyright (C) 2024-2025 raould@gmail.com License: GPLv2 / GNU General. Public License, version 2. https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html */
+
+const assertionsEnabled: boolean = (() => {
+    const is_node = typeof process !== "undefined";
+    return is_node && process.env.GAME_DEBUG == "1";
+})();
+
 export function time_str(date: Date): string {
     return date.toLocaleTimeString('en-US', { hour12: false });
 }
@@ -77,42 +83,38 @@ function msgs2strs(...msgs: any): string {
 }
 
 export function assert_fail(...msgs: any) {
-    if (process.env.GAME_DEBUG !== "1") {
-	return;
+    if (assertionsEnabled) {
+	const strs = msgs2strs(msgs);
+	log_stamp(
+            "ASSERTION FAILED:",
+	    ...strs,
+            "\n",
+            new Error().stack
+	);
     }
-    const strs = msgs2strs(msgs);
-    log_stamp(
-        "ASSERTION FAILED:",
-	...strs,
-        "\n",
-        new Error().stack
-    );
 }
 
 export function assert(test: boolean, ...msgs: any) {
-    if (process.env.GAME_DEBUG !== "1") {
-	return;
-    }
-    if (!test) {
-        assert_fail(...msgs);
+    if (assertionsEnabled) {
+	if (!test) {
+            assert_fail(...msgs);
+	}
     }
 }
 
 export function assert_fn(a: any, b: any, compare: (a:any, b:any)=>boolean, ...msgs: any) {
-    if (process.env.GAME_DEBUG !== "1") {
-	return;
-    }
-    const eq = compare(a, b);
-    if (!eq) {
-        assert_fail(...msgs, a, b);
+    if (assertionsEnabled) {
+	const eq = compare(a, b);
+	if (!eq) {
+            assert_fail(...msgs, a, b);
+	}
     }
 }
 
 export function assert_eqeq(a: any, b: any, ...msgs: any) {
-    if (process.env.GAME_DEBUG !== "1") {
-	return;
-    }
-    if (a !== b) {
-        assert_fail(...msgs, a, b);
+    if (assertionsEnabled) {
+	if (a !== b) {
+            assert_fail(...msgs, a, b);
+	}
     }
 }
