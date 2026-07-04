d2cef1e3acb176fcbd5de9ac314a4b3fe6954946
Raoul Duke <raould@gmail.com>
2026-07-03 20:55:40 -0700

    copyright

--- OLD/server/src/animation.ts
+++ OLD/server/src/animation.ts
@@ -1,4 +1,4 @@
-/* Copyright (C) 2024-2025 raould@gmail.com License: GPLv2 / GNU General. Public License, version 2. https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html */
+/* Copyright (C) 2024-2026 raould@gmail.com License: GPLv2 / GNU General. Public License, version 2. https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html */
 import * as GDB from './game_db';
 import * as S from './sprite';
 import * as G from './geom';
